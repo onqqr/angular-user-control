1a70c86f35e8db4e910276e2dfc556ba3c0e72af
onqqr <dnbyaniss@gmail.com>
2024-08-15 13:58:37 +0300

    feat: add login component

--- angular-user-control/src/app/app.component.ts
+++ angular-user-control/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginPageComponent } from "./components/login-page/login-page.component";
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-root',
--- angular-user-control/src/app/components/login-page/login-page.component.ts
+++ angular-user-control/src/app/components/login-page/login-page.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatInputModule, MatButtonModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-  title = 'hello world';
+  @Output() submitEM = new EventEmitter();
+
+  public form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+    }
+  }
 }
