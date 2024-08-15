20e02ba86da6c002e9894db30674c96d54c66d2e
onqqr <dnbyaniss@gmail.com>
2024-08-15 14:51:01 +0300

    feat: add base component

--- angular-user-control/src/app/app.component.ts
+++ angular-user-control/src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent],
+  imports: [RouterOutlet, RouterLink, LoginPageComponent, MatButtonModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-user-control';
 }
--- angular-user-control/src/app/app.routes.ts
+++ angular-user-control/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+
+  { path: '', component: AppComponent }
+];
